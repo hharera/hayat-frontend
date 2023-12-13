f9f16c9cef1f31a31d491753e87602ac03026b4c
Hassan Harera <Hassan.Shaban.Harera@gmail.com>
2023-12-13 13:47:21 +0200

    initial commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  onHomeClicked() {
+
+  }
+}
