00a7f135156f1af360f62740ba7e9cc9461014ff
hharera <hassan.shaban.harera@gmail.com>
2024-01-01 10:09:50 +0200

    add xd files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
+import {HeaderComponent} from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, CommonModule, RouterOutlet],
+  imports: [HomeComponent, CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
